f5267b85ed3207a2f2586715e8fbf8beb4ea3624
joshp423 <205880399+joshp423@users.noreply.github.com>
2026-01-20 11:34:42 +0000

    Started writing controller function for add form

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -16,11 +16,24 @@ async function createStockGet (req, res) {
 };
 
 const { body, validationResult, matchedData } = require("express-validator");
+const lengthErrTitle = "must be between 1 and 25 characters.";
+const numErrPrice = "must be above $1.";
+const numErrQuantity = "must be between 1 and 2000 units.";
+const isAlphaBrand = "must be a brand.";
 
 const validateMessage = [
-  body("")
+  body("productTitle").trim().escape()
+    .isLength({min: 1, max: 200}).withMessage(`Product title ${lengthErrTitle}`),
+  body("productPrice").escape()
+    .isCurrency({allow_negatives: 'false'})
+    .isNumeric().isInt({min: 1})
+  
 ]
 
 module.exports = {
   allStockGet,
+  createStockPost: [
+    validateMessage,
+    async function 
+  ]
 }
