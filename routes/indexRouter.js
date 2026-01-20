6a0779676340089d65e40f5bb8eabaf4ea0cbad7
joshp423 <205880399+joshp423@users.noreply.github.com>
2026-01-20 16:42:31 +1300

    setup of db and views pages

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -2,7 +2,6 @@ const { Router } = require("express");
 const indexController = require("../controllers/indexController");
 const indexRouter = Router();
 
-
-
+indexRouter.get("/", indexController.getStock);
 
 module.exports = indexRouter;
