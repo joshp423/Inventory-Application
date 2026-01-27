b208e0603a576fe0482a43026d0e867c45cfe73a
joshp423 <205880399+joshp423@users.noreply.github.com>
2026-01-27 21:29:13 +0000

    render and neon requirements added

--- app.js
+++ app.js
@@ -13,6 +13,30 @@ app.set("view engine", "ejs");
 app.use( "/", indexRouter );
 app.use(express.static('public'));
 
+require('dotenv').config();
+const { Pool } = require('pg');
+const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD } = process.env;
+const pool = new Pool({
+  host: PGHOST,
+  database: PGDATABASE,
+  username: PGUSER,
+  password: PGPASSWORD,
+  port: 5432,
+  ssl: {
+    require: true,
+  },
+});
+async function getPgVersion() {
+  const client = await pool.connect();
+  try {
+    const result = await client.query('SELECT version()');
+    console.log(result.rows[0]);
+  } finally {
+    client.release();
+  }
+}
+getPgVersion();
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, (error) => {
     if (error) {
